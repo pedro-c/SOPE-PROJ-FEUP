9a946ab457bfae8caec030f125cceb5bf6458b41
Pedro Costa <pedrodc@outlook.com>
2016-03-31 12:51:19 +0100

    dar tudo

--- rmdup.c
+++ rmdup.c
@@ -5,14 +5,13 @@
 #include <sys/stat.h>
 #define TRUE 1
 #define FALSE 0
+#define MAX 256
 //ambos são ficheiros regulares;
 //o ambos têm o mesmo nome;
 //o ambos têm as mesmas permissões de acesso;
 //o ambos têm o mesmo conteúdo.
 
-void saveToFile(){
 
-}
 
 int compareContent(FILE *first, FILE *second)
 {
@@ -39,111 +38,42 @@ int compareContent(FILE *first, FILE *second)
 
 }
 
-void compareFiles(FILE *src){
+void compareFiles(char *files[]){
 
-  FILE *first, *second, *copy;
-  char *fileName1[], *fileName2[], *dirName1[], *dirName2[], *size1[], *size2[], *permissions1[], *permissions2[];
-
-  while(!src.eof())
-  {
-    getline(src, fileName1);
-    getline(src, dirName1);
-    getline(src, size1);
-    getline(src, permissions1);
-    copy = src;
-    while(!copy.eof())
-    {
-      getline(src, fileName2);
-
-      if(fileName1 != fileName2){
-        continue;
-      }
-
-      getline(src, dirName2);
-
-      if(permissoes1 != permissions2){
-        continue;
-      }
-
-      getline(src, size2);
-      if(size1 != size2){
-        continue;
-      }
-
-      first = fopen(dirName1 + fileName1);
-      second = fopen(dirName2 + fileName2);
-      if(compareContent(first, second))
-      {
-        //update do ficheiro
-      }
-    }
-  }
 }
 
 
-int main(int argc, char *argv[]) {
-    DIR *dirp;
-    char *dir[];
-    struct dirent *direntp;
-    char filenameTmp;
-    struct stat stat_buf;
-    int flag = TRUE;
-    FILE *destination;
 
-    if (argc != 2) {
-      fprintf(stderr, "Invalid Arguments");
-      exit(1);
-    }
-
-    if ((dirp = opendir(argv[1])) == NULL) {
-      perror(argv[1]);
-      exit(2);
-    }
-
-    dir = argv[1];
-
-
-    int pid=fork();
-    int pid2 = 1;
-
-    if(pid>0) //pai, compares and changes files that are equal
-    {
-      wait();
-      compareFiles(destination);
-    }
-    else //child, reads the directories and adds the files to a text file
-    {
-      while((direntp = readdir(dirp)) != NULL) //reads all directory files
-      {
-        if(pid > 0){
-          flag = FALSE;
-        }
-
-        if(flag && pid2 = 0){
-          closedir(dirp);
-          dirp = opendir(filenameTmp);
-          flag = FALSE;
-        }
-
-        if (lstat(direntp->d_name, &stat_buf)==-1){
-          perror("lstat");
-          exit(3);
-        }
-
-        if (S_ISREG(stat_buf.st_mode)){
-          //falta sincronizar
-            saveToFile(direntp, dirp);
-        }
-
-        else if (S_ISDIR(stat_buf.st_mode)){
-          filenameTmp = stat_buf;
-          flag = TRUE;
-          pid2 = fork();
-      }
-
-
-      }
-      closedir(dirp);
-
-    }
+int createProcess(char *fileName, char *files)
+{
+  int pid = fork();
+  if(pid == 0)
+  {
+    execlp("listdir", "listdir", fileName, files, NULL);
+    perror("execlp");
+    exit(4);
   }
+  return pid;
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+
+
+  if (argc != 2) {
+    fprintf(stderr, "Invalid Arguments");
+    exit(1);
+  }
+
+  char files[] = "\"To Save\" File";
+
+  if(createProcess(argv[1], files) > 0)
+  {
+    wait(); // a completar
+    compareFiles(files);
+  }
+
+  return 0;
+}
